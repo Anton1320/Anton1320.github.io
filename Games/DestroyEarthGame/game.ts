85003fb3b0739ca8de0e6d24750f3b034b554a10
Anton <56134229+Anton1320@users.noreply.github.com>
2022-10-30 22:40:57 +0300

    Update game.ts

--- Games/DestroyEarthGame/game.ts
+++ Games/DestroyEarthGame/game.ts
@@ -2,6 +2,22 @@ let canvas = <HTMLCanvasElement>document.getElementById("myCanvas");
 
 let ctx = canvas.getContext("2d");
 
+let drawOrigin = {x: 0, y: 0};
+let drawScale = 1;
+
+function restrict(l: number, n: number, r: number): number {
+    if (n <= l) return l;
+    if (n >= r) return r;
+    return n;
+}
+
+function rnd(max: number): number;
+function rnd(min: number, max: number): number;
+function rnd(minOrMax?: number, max?: number): number {
+    if (max !== null) return Math.floor(Math.random() * (max - minOrMax) + minOrMax);
+    return Math.floor(Math.random() * minOrMax);
+  }
+
 type vector = {x: number, y: number};
 
 function sum(l: vector, r: vector): vector {
@@ -32,18 +48,26 @@ abstract class GameObject {
     abstract draw(): void;
 }
 
-class Body extends GameObject {
-    mass: number = 1;
+class Circle extends GameObject {
     radius: number = 1;
     color: typeof ctx.fillStyle = "white";
-    constructor(x: number, y: number, mass: number, color: typeof ctx.fillStyle) {
+    constructor (x: number, y: number, radius: number, color: typeof ctx.fillStyle) {
         super(x, y);
-        this.mass = mass;
-        this.radius = mass/4;
-        this.color = color;
+        this.radius = radius
+        this.color = color
     }
     draw() {
-        drawCircle(this.pos.x, this.pos.y, this.radius, this.color);
+        drawCircle((this.pos.x-drawOrigin.x)*drawScale, (this.pos.y-drawOrigin.y)*drawScale, this.radius*drawScale, this.color);
+    }
+}
+
+class Body extends Circle {
+    mass: number = 1;
+    isTarget = false;
+    constructor(x: number, y: number, mass: number, color: typeof ctx.fillStyle, isTarget = false) {
+        super(x, y, mass/4, color);
+        this.mass = mass;
+        this.isTarget = isTarget;
     }
     colide(r: Body) {
         return len(sub(this.pos, r.pos)) <= this.radius + r.radius;
@@ -78,6 +102,16 @@ function drawCircle(x: number, y: number, r: number, fillStyle: typeof ctx.fillS
     ctx.closePath();
 }
 
+function drawLine(start: vector, end: vector, color: typeof ctx.fillStyle) {
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke(); 
+    ctx.strokeStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
+
 var planets: Body[] = [
     new Body(canvas.width*2/3, canvas.height/3, 100, "red"),
     new Body(canvas.width/3, canvas.height*2/3, 100, "red")
@@ -86,25 +120,56 @@ var asteroid = new Asteroid(new Body(0, 0, 30, "green"));
 asteroid.speed = {x: 1, y: 0};
 
 var asteroidIsSet = false;
+var globalAsteroidSetPos = {x: 0, y: 0};
+
 var asteroidIsFired = false;
 
+var frameCounter = 0;
+
+var pathPoints: Circle[] = []
+
+let scoreText = document.getElementById("score");
+var score = 0;
+
+var attemtCounter = -1;
+
 function reset() {
+    attemtCounter++;
+    scoreText.textContent = score.toString() + " / " + attemtCounter.toString();
     asteroidIsSet = false;
     asteroidIsFired = false;
+    frameCounter = 0;
+    drawOrigin = {x: 0, y: 0};
+    drawScale = 1;
+    pathPoints = []
+
+    asteroid.body.pos = {x: -asteroid.body.radius, y: -asteroid.body.radius};
+
+    let planetsNum = rnd(3, 6);
+    planets = []
+    for (let i = 0; i < planetsNum; ++i) {
+        planets.push(new Body(rnd(canvas.width/8, canvas.width*7/8), rnd(canvas.height/8, canvas.height*7/8), rnd(50, 200), "red"));
+        console.log(planets[i])
+    }
+    planets.push(new Body(rnd(canvas.width/8, canvas.width*7/8), rnd(canvas.height/8, canvas.height/8), rnd(50, 200), "blue", true))
 }
 
 document.addEventListener("mousemove", mousemoveHandler, false);
 document.addEventListener("mousedown", mousedownHandler, false);
 document.addEventListener("mouseup", mouseupHandler, false);
-
+var globalMousePos = {x: 0, y: 0};
 function mousemoveHandler(e: MouseEvent) {
-    var mousePos = {x: e.offsetX, y: e.offsetY};
+    
+    globalMousePos = {x: e.screenX, y: e.screenY};
+    var mousePos = {x: e.screenX-canvas.offsetLeft, y: e.screenX-canvas.offsetTop};
     if (!asteroidIsFired) {
         if (!asteroidIsSet) {
-            asteroid.body.pos = mousePos;
+            asteroid.body.pos.x = restrict(asteroid.body.radius, mousePos.x, canvas.width-asteroid.body.radius);
+            asteroid.body.pos.y = canvas.height-asteroid.body.radius;
+            globalAsteroidSetPos = globalMousePos;
         }
         else {
-            asteroid.speed = mul(sub(asteroid.body.pos, mousePos), 0.05);
+            asteroid.speed = mul(sub(globalAsteroidSetPos, globalMousePos), 0.05);
         }
     }
 }
@@ -122,15 +187,48 @@ function mouseupHandler(e: MouseEvent) {
         asteroidIsFired = true;
     }
 }
-
+reset();
 setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (frameCounter > 1000) reset();
+    if (asteroid.body.pos.x >3*canvas.width || asteroid.body.pos.x < -3*canvas.width) reset();
+    if (asteroid.body.pos.y >3*canvas.height || asteroid.body.pos.y < -3*canvas.height) reset();
     if (asteroidIsFired) asteroid.move(planets);
     for (var planet of planets) {
         planet.draw();
-        if (planet.colide(asteroid.body)) {
+        if (planet.colide(asteroid.body) && asteroidIsFired) {
+            if (planet.isTarget) {
+                score++;
+                scoreText.textContent = score.toString();
+            }
             reset();
         }
     }
+
+    for (let point of pathPoints) {
+        point.draw();
+    }
+
+    drawScale = 1;
+    drawOrigin = {x: 0, y: 0};
+    if (asteroidIsFired) {
+        if (asteroid.body.pos.x > canvas.width) drawScale = Math.min(canvas.width/asteroid.body.pos.x, drawScale);
+        if (asteroid.body.pos.y > canvas.height) drawScale = Math.min(drawScale, canvas.height/asteroid.body.pos.y, drawScale);
+        if (asteroid.body.pos.x < 0) drawScale = Math.min(canvas.width/(canvas.width-asteroid.body.pos.x), drawScale);
+        if (asteroid.body.pos.y < 0){
+            drawScale = Math.min(canvas.height/(canvas.height-asteroid.body.pos.y), drawScale);
+        }
+        if (asteroid.body.pos.x < 0) drawOrigin.x = asteroid.body.pos.x;
+        if (asteroid.body.pos.y < 0) drawOrigin.y = asteroid.body.pos.y;
+
+        if (frameCounter % 7 == 0) {
+            pathPoints.push(new Circle(asteroid.body.pos.x, asteroid.body.pos.y, 2, "black"));
+        }
+    }
+    if (asteroidIsSet) {
+        drawLine(asteroid.body.pos, sum(asteroid.body.pos, sub(globalAsteroidSetPos, globalMousePos)), "green");
+    }
+    frameCounter++;
+
     asteroid.body.draw();
 }, 10);
